3e695771f09f37a734f11819bf1b535207cfcf07
Denis Rastrepin <rusBeliy161@yandex.ru>
2024-04-21 20:12:28 +0300

    level-1: изменение cors

--- app.js
+++ app.js
@@ -16,20 +16,10 @@ const app = express();
 app.use(express.json());
 app.use(helmet());
 
-const options = {
-  origin: [
-    'http://localhost:3000',
-    'http://localhost:3001',
-    'http://rd-movies.nomoredomainswork.ru',
-    'https://rd-movies.nomoredomainswork.ru',
-  ],
-  methods: ['GET', 'HEAD', 'PUT', 'PATCH', 'POST', 'DELETE'],
-  preflightContinue: false,
-  optionsSuccessStatus: 204,
-  allowedHeaders: ['Content-Type', 'origin', 'Authorization'],
+app.use(cors({
+  origin: 'https://rd-movies.nomoredomainswork.ru',
   credentials: true,
-};
-app.use(cors(options));
+}));
 app.use(limiter);
 app.use(requestLogger);
 app.use(appRouter);
